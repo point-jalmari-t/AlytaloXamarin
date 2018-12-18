cf4816305a7e198c60f8380e3e9c2539da50346c
Jalmari Talvipuro <jalmari.talvipuro@stud.pointcollege.fi>
2018-12-18 10:26:07 +0200

    asd

--- AlytaloXamarin/AlytaloXamarin/MainPage.xaml.cs
+++ AlytaloXamarin/AlytaloXamarin/MainPage.xaml.cs
@@ -9,9 +9,28 @@ namespace AlytaloXamarin
 {
     public partial class MainPage : ContentPage
     {
+        private int saunanLampotila;
+        private string saunaPaalla;
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void Sauna_clicked(object sender, EventArgs e)
+            {
+            SaunanNykyLampotila.Text = "Tähän tulee lämpötila";
+            SaunanTila.Text = "Pällä/pois";
+            }
+        private void Valot_clicked(object sender, EventArgs e)
+        {
+            ValonTila.Text = "Pällä/pois";
+            ValonMaara.Text = "tähän tulee kirkkaus 0-10";
+        }
+        private void Termostaatti_clicked(object sender, EventArgs e)
+        {
+            TalonTavoiteLampotila.Text = "Celsius";
+            TalonNykyLampotila.Text = "Celsius";
         }
     }
 }
